7b2a6b028c954400551fc05532b1f147f63a6aa1
JoeYip13 <joeyip13@hotmail.co.uk>
2023-04-03 17:23:46 +0000

    Create username function to get player username

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+""" ----- A game of Battleships. -----"""
+# A single player game against the computer.
+# Rules of the game;
+# 1. Enter your username and the game will begin.
+# 2. Gameboard will automatically generate 5 random ship spaces for the user
+# and computer.
+# 3. Both game boards will be dsplayed. User and Computer.
+# 4. Enter your X and Y co-ordinates.
+# 6. Game objectives is to sink the opponents ship.
+# 7. Winner is first to sink all opponents ships.
+
+
+def username():
+    """
+    Gets the user name
+    """
+    user = input('Enter your username: ')
+    print(f"Welcome to Battleships Admiral {user}!")
+
+
+username()
